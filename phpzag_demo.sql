03ab017880f14731501861d49a3bef84e92b49c0
sarveshpatel <sarvesh.patel@cre8ivelabs.com>
2021-09-15 12:51:04 +0530

    added

---
+++ phpzag_demo.sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 15, 2021 at 09:20 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpzag_demo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_order`
+--
+
+CREATE TABLE `invoice_order` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `order_receiver_name` varchar(250) NOT NULL,
+  `order_receiver_address` text NOT NULL,
+  `order_total_before_tax` decimal(10,2) NOT NULL,
+  `order_total_tax` decimal(10,2) NOT NULL,
+  `order_tax_per` varchar(250) NOT NULL,
+  `order_total_after_tax` double(10,2) NOT NULL,
+  `order_amount_paid` decimal(10,2) NOT NULL,
+  `order_total_amount_due` decimal(10,2) NOT NULL,
+  `note` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoice_order`
+--
+
+INSERT INTO `invoice_order` (`order_id`, `user_id`, `order_date`, `order_receiver_name`, `order_receiver_address`, `order_total_before_tax`, `order_total_tax`, `order_tax_per`, `order_total_after_tax`, `order_amount_paid`, `order_total_amount_due`, `note`) VALUES
+(2, 123456, '2021-01-31 14:03:42', 'abcd', 'Admin\r\nA - 4000, Ashok Nagar, New Delhi,\r\n 110096 India.\r\n12345678912\r\nadmin@phpzag.com', '342400.00', '684800.00', '200', 1027200.00, '45454.00', '981746.00', 'this note txt'),
+(682, 123456, '2021-09-10 11:55:00', 'Test', 'sdfdsf', '157.00', '0.00', '0', 157.00, '157.00', '0.00', 'sadsdwsdsd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_order_item`
+--
+
+CREATE TABLE `invoice_order_item` (
+  `order_item_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `item_code` varchar(250) NOT NULL,
+  `item_name` varchar(250) NOT NULL,
+  `order_item_quantity` decimal(10,2) NOT NULL,
+  `order_item_price` decimal(10,2) NOT NULL,
+  `order_item_final_amount` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoice_order_item`
+--
+
+INSERT INTO `invoice_order_item` (`order_item_id`, `order_id`, `item_code`, `item_name`, `order_item_quantity`, `order_item_price`, `order_item_final_amount`) VALUES
+(4100, 2, '13555', 'Face Mask', '120.00', '2000.00', '240000.00'),
+(4101, 2, '34', 'mobile', '10.00', '10000.00', '100000.00'),
+(4102, 2, '34', 'mobile battery', '1.00', '34343.00', '34343.00'),
+(4103, 2, '34', 'mobile cover', '10.00', '200.00', '2000.00'),
+(4104, 2, '36', 'testing', '1.00', '2400.00', '2400.00'),
+(4364, 682, '1', '2 papers', '2.00', '23.00', '46.00'),
+(4365, 682, '2', 'sdsd', '1.00', '111.00', '111.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_user`
+--
+
+CREATE TABLE `invoice_user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `mobile` bigint(20) NOT NULL,
+  `address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoice_user`
+--
+
+INSERT INTO `invoice_user` (`id`, `email`, `password`, `first_name`, `last_name`, `mobile`, `address`) VALUES
+(123456, 'admin@phpzag.com', '12345', 'Admin', '', 12345678912, 'New Delhi 110096 India.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `invoice_order`
+--
+ALTER TABLE `invoice_order`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `invoice_order_item`
+--
+ALTER TABLE `invoice_order_item`
+  ADD PRIMARY KEY (`order_item_id`);
+
+--
+-- Indexes for table `invoice_user`
+--
+ALTER TABLE `invoice_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `invoice_order`
+--
+ALTER TABLE `invoice_order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=683;
+
+--
+-- AUTO_INCREMENT for table `invoice_order_item`
+--
+ALTER TABLE `invoice_order_item`
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4366;
+
+--
+-- AUTO_INCREMENT for table `invoice_user`
+--
+ALTER TABLE `invoice_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123457;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
